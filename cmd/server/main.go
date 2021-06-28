339b24d42fdd1047a5f14790289007ee39209b47
Paddy Gallagher <paddy.gallagher@gmail.com>
2021-06-28 23:44:14 +0100

    Initial code organisation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,23 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things
+// like pointers to database connections
+type App struct {}
+
+// Run - sets up Arranmore REST API
+func (app *App) Run() error  {
+	fmt.Println("Setting up Arranore REST API")
+	return nil
+}
+
 func main()  {
-	fmt.Println("Go Arranmore API")
+	fmt.Println("Arranmore REST API")
+	app := App{}
+	if err := app.Run; err != nil {
+		fmt.Println("Error starting up Arranmore REST API")
+		fmt.Println(err)
+	}
 }
 
 
