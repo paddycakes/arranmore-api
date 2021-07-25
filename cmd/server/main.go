93ba6dd8fa9a8ed605d0241eb859dc816f6c3fad
Paddy Gallagher <paddy.gallagher@gmail.com>
2021-07-25 18:48:40 +0100

    Get port from environment if available or else use default

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,9 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/paddycakes/arranmore-api/internal/sensor"
 	transportHTTP "github.com/paddycakes/arranmore-api/internal/transport/http"
+	"log"
 	"net/http"
+	"os"
 )
 
 // App - the struct which contains things
@@ -34,17 +36,21 @@ func (app *App) Run() error  {
 	// origins := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
 	origins := handlers.AllowedOrigins([]string{"*"})
 
-	// TODO: Need to sort out the port here .. may need to get from environment. Issue when running locally as both FE/BE are on :8080
-	if err := http.ListenAndServe(":1234", handlers.CORS(headers, methods, origins)(handler.Router)); err != nil {
-		fmt.Println("Failed to setup server")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	if err := http.ListenAndServe(":"+port, handlers.CORS(headers, methods, origins)(handler.Router)); err != nil {
 		return err
 	}
+	log.Println("Arranmore REST API successfully started and listening on port ", port)
+	// 	fmt.Println("Arranmore REST API")
 
 	return nil
 }
 
 func main()  {
-	fmt.Println("Arranmore REST API")
 	app := App{}
 	if err := app.Run(); err != nil {
 		fmt.Println("Error starting up Arranmore REST API")
